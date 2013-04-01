f4c50d02a03a71e5dbf3f7a81db7283e23371061
itsjoesullivan <itsjoesullivan@gmail.com>
2013-04-01 16:08:54 -0400

    background

---
+++ background.js
@@ -0,0 +1,10 @@
+chrome.app.runtime.onLaunched.addListener(function(launchData) {
+    console.log(arguments);
+  chrome.app.window.create('index.html', {
+    id: '606'
+  }, function(w) {
+      w.contentWindow.win = w;
+      w.contentWindow.launchData = launchData;
+      w.show();
+  });
+});
